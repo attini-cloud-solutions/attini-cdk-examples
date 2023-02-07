0ec3b713364d921152923be11dc23bd1ed027e05
oscarostrand <oscar.ostrand@gmail.com>
2023-02-07 17:44:18 +0100

    Added js example

---
+++ javascript/deployment-plan-app/test/deployment-plan-app.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const DeploymentPlanApp = require('../lib/deployment-plan-app-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deployment-plan-app-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new DeploymentPlanApp.DeploymentPlanAppStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
